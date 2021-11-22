32faaf958c790b3bbd2a512e6848cb0fa7392d9e
Zhenbo Li <lizhenbo@yahoo.com>
2021-11-22 13:35:58 +0800

    structure

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+mod nd;
+
+enum CellStatus {
+    Empty,
+    Human,
+    Box,
+}
+
+pub struct Graph {
+
+}
+
+pub struct GameStatus {
+
+}
+
+pub fn build_game_status(g:Graph) -> GameStatus {
+    let mut st = GameStatus{};
+    return st;
+}
+
+pub fn get_input_map() -> Graph {
+    let mut g = Graph{};
+    return g;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use push_box::{get_input_map, build_game_status, GameStatus};
+use std::collections::VecDeque;
+
+
 fn main() {
     println!("Hello, world!");
+    let ori_graph = get_input_map();
+    let ori_status = build_game_status(ori_graph);
+    let mut bfs_queue = VecDeque::new();
+
+    bfs_queue.push_back(ori_status);
+    loop {
+        // println!("ok");
+        let g = bfs_queue.pop_front();
+        let mut st;
+        match  g {
+            None => break,
+            Some(v) => st = v,
+        }
+
+        try_extend(&mut bfs_queue, st);
+    }
+}
+
+fn try_extend(q: &mut VecDeque<GameStatus>, st: GameStatus) {
+    todo!()
 }
